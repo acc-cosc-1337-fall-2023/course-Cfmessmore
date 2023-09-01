09113ed09419544cd9796976fa953dd3f85ca532
Cfmessmore <143145296+Cfmessmore@users.noreply.github.com>
2023-09-01 01:27:30 +0000

    hw1:multiplying numbers func and test cases

--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -2,15 +2,25 @@
 #include "catch.hpp"
 
 TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+	//REQUIRE(true == true);
 }
 
 /*test case echo_variable */
-
+TEST_CASE("test echo_variable function", "function should return the same value as the function argument"){
+		//REQUIRE(5 == echo_variable(5));
+		//REQUIRE(50 ==echo_variable(50));
+		//REQUIRE(500 == echo_variable(500));
+}
 /*test case add_to_double_1 with 0 as parameter*/
-
+TEST_CASE("Test the add to double 1 function", "testing the decimal inconsistencies")
+{
+	//REQUIRE(.9 == add_to_double1(0));
+}
 /*test case add_to_double_1 with 1 as parameter*/
-
+TEST_CASE("Test the add to double 2 function", "testing the decimal inconsistencies")
+{
+	//REQUIRE(1.5 == add_to_double_2(0));
+}
 /*test case add_to_double_2 with 0 as parameter*/
 
 /*test case add_to_double_2 with 1 as parameter*/
